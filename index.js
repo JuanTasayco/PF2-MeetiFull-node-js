2f474882298b4f7a665d545b6673ff6a8802e4c7
JuanTasayco <juantasayco266@gmail.com>
2022-09-23 01:01:45 -0500

    v0.1

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require("express"); //importar se usa require
 const cors = require("cors");
-const  path = require("path");
+const path = require("path");
 
 const { dbConnection } = require("./db/config");
+
 require("dotenv").config(); // TOMA CONFIGURACION POR DEFECTO CUANDO CARGA DEL ARCHIVO ENV
-
-
 
 const app = express();
 
 dbConnection(); // CONEXION A LA BASE DE DATOS EN INDEX.JS
 
-
 app.use(express.static("public"));
 
 app.use(cors()); // si sabemos que dominio tenemos lo podemos definir aquí ayudando a la protección
+
 app.use(express.json());  // RESPETAR EL ORDEN DEBEN ESTAR ANTES QUE LA DEFINICION DE RUTAS
 
 app.use("/api/auth", require("./routes/auth"))      //CUANDO ALGUIEN HAGA UNA PETICION A ESTA DIRECCION
